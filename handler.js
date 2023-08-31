9e02be691054ed7c3a60fa60cca178f2f16b4c52
osneidermillan <boteromillanosneider@gmail.com>
2023-08-30 22:35:15 -0500

    second commit with querystring aws

--- handler.js
+++ handler.js
@@ -1,20 +1,23 @@
 'use strict';
+const querystring = require("querystring")
 const mysql=require('mysql');
+const { parse } = require('path');
 const connection=mysql.createConnection({
   host:'finalpedidosaws.c9jwd0itecy9.us-east-1.rds.amazonaws.com',
   user:'admin',
   port:"3306",
   pasword:'1234567890',
   database:'finalawsdo',
-})
+});
 
 module.exports.realizarfinalpedidosaws = async (event) => {
+  const pedido = querystring.parse(event["body"])
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: "success",
+        input: `peticion ${pedido.name}`,
       },
       null,
       2
